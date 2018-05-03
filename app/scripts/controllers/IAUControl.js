4404b628119ee5ed19530f65d77bdc3330438616
Larry Fritz <llfritz@drbsystems.com>
2018-05-03 14:31:53 -0400

    Coalesced jumbotron labels, fixed card height, added test card

--- app/scripts/controllers/IAUControl.js
+++ app/scripts/controllers/IAUControl.js
@@ -3,6 +3,16 @@ var app = angular.module('IAUApp',[]);
 app.controller('IAUController', function MainCtrl($scope, $http){
   //fun stuuf here
   $scope.apiURL = "http://130.101.92.77/NasaSuits/api/eva/iau/GetIAU.php?iau_id=";
+  var video_iu = {
+    iau_id: "1893",
+    iu_id: "1498",
+    iu_link: "https://www.youtube.com/watch?v=Z62z64-AyH0",
+    iu_position: "0,0,0",
+    iu_rotation: "0,0,0",
+    iu_scale: "1,1,1",
+    iu_space: "EVA_SPHERE_SPACE",
+    iu_type: "video"
+  };
 
   $scope.getIAU = function(id){
     $http({
@@ -12,6 +22,7 @@ app.controller('IAUController', function MainCtrl($scope, $http){
         function success(result){
 
         $scope.IAU = result.data;
+        $scope.IAU.iau_ius.push(video_iu);
         console.log($scope.IAU);
       },
        function failure(error){
@@ -20,4 +31,5 @@ app.controller('IAUController', function MainCtrl($scope, $http){
   };
 
   $scope.getIAU(1893);
+
 });
